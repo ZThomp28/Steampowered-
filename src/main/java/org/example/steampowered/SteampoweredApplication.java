a9cb69f4a416c787ccf9c5c5132b918df5c95399
Paul Berger <paulberger56@gmail.com>
2024-02-05 13:37:27 -0500

    Add initial code base

---
+++ src/main/java/org/example/steampowered/SteampoweredApplication.java
@@ -0,0 +1,13 @@
+package org.example.steampowered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteampoweredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteampoweredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/steampowered/SteampoweredApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.steampowered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteampoweredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
