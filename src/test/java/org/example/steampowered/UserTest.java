dc10e4a0b4944016b00328d8a9fc0905ba4a6c56
Paul Berger <paulberger56@gmail.com>
2024-02-06 17:04:17 -0500

    Build UserTest for The User Java class

---
+++ src/test/java/org/example/steampowered/UserTest.java
@@ -0,0 +1,26 @@
+package org.example.steampowered;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+class UserTest {
+
+    User paul = new User("WebsiteMindOfPaul", "MindOfPaul", "1111");
+
+    @Test
+    void getWebsiteUserName() {
+        Assertions.assertEquals("WebsiteMindOfPaul", paul.getWebsiteUserName());
+    }
+
+    @Test
+    void getSteamUserName() {
+        Assertions.assertEquals("MindOfPaul", paul.getSteamUserName());
+    }
+
+    @Test
+    void getSteamID() {
+        Assertions.assertEquals("1111", paul.getSteamID());
+    }
+}
