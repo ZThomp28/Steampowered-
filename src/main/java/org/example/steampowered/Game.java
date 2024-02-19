c156c9a6bd0210de8be0f936184f73afe3226275
Albert <albert.austiniv@gmail.com>
2024-02-19 11:19:32 -0500

    Some work on game class

---
+++ src/main/java/org/example/steampowered/Game.java
@@ -0,0 +1,38 @@
+package org.example.steampowered;
+
+public class Game {
+
+    private String appid;
+    private String name;
+    private String imgIconURL;
+
+    public Game(String appid, String name, String imgIconURL) {
+        this.appid = appid;
+        this.name = name;
+        this.imgIconURL = imgIconURL;
+    }
+
+    public String getAppid() {
+        return appid;
+    }
+
+    public void setAppid(String appid) {
+        this.appid = appid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgIconURL() {
+        return imgIconURL;
+    }
+
+    public void setImgIconURL(String imgIconURL) {
+        this.imgIconURL = imgIconURL;
+    }
+}
---
+++ src/test/java/org/example/steampowered/GameTest.java
@@ -0,0 +1,20 @@
+package org.example.steampowered;
+
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+    Game cs2 = new Game("730", "Counter-Strike 2", "asdasd");
+
+    @Test
+    void getAppid() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getImgIconURL() {
+    }
+}
