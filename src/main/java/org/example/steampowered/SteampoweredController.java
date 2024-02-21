936220f72b6f188d70754ccc8689f781c72ff0c2
Julian <jramirez28@ggc.edu>
2024-02-21 12:05:39 -0500

    Update on the local pathing to see openID redirect back to tester page.

--- src/main/java/org/example/steampowered/SteampoweredController.java
+++ src/main/java/org/example/steampowered/SteampoweredController.java
@@ -25,4 +25,21 @@ public class SteampoweredController {
     public String getProfilePage(){
         return "profile";
     }
+	@RequestMapping(value="/redirect", method = RequestMethod.GET)
+    public ModelAndView runManager(){
+        OpenIdManager manager = new OpenIdManager();
+        manager.setReturnTo("http://localhost:8080");
+        manager.setRealm("http://localhost:8080");
+
+        Endpoint endpoint = manager.lookupEndpoint("https://steamcommunity.com/openid/");
+        System.out.println(endpoint);
+
+        Association association = manager.lookupAssociation(endpoint); 
+        System.out.println(association);
+
+        String url = manager.getAuthenticationUrl(endpoint, association);
+        System.out.println("Copy the authentication URL in browser:\n" + url);
+
+        return new ModelAndView("redirect:" + url);
+    }
 }
