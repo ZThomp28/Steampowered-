26237e2e4eeafd0b92e38e182b17e0da8c7a6256
Paul Berger <paulberger56@gmail.com>
2024-02-22 18:10:17 -0500

    Added logic to grab openID values

--- src/main/java/org/example/steampowered/SteampoweredController.java
+++ src/main/java/org/example/steampowered/SteampoweredController.java
@@ -1,19 +1,53 @@
 package org.example.steampowered;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.expressme.openid.Association;
 import org.expressme.openid.Endpoint;
 import org.expressme.openid.OpenIdManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SteampoweredController {
 
     @GetMapping("/")
-    public String getIndexPage(){
+    public String getIndexPage(HttpServletRequest request, Model model){
+        // When the openID returns the user to the index page, grab the values from the nav bar
+        Map<String, String[]> parameterMap = request.getParameterMap();     
+        
+        // Filter out only the parameters openid.claimed_id and openid.identity
+        Map<String, String[]> filteredParameters = new HashMap<>();
+        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+            String paramName = entry.getKey();
+            if ("openid.claimed_id".equals(paramName) || "openid.identity".equals(paramName)) {
+                filteredParameters.put(paramName, entry.getValue());
+            }
+        }       
+
+        // print out the values to verify that it worked
+        if(filteredParameters.size() > 0){
+            for (Map.Entry<String, String[]> entry : filteredParameters.entrySet()) {
+                String paramName = entry.getKey();
+                String[] paramValues = entry.getValue();
+    
+                System.out.println("Parameter: " + paramName);
+                System.out.print("Values: ");
+                for (String value : paramValues) {
+                    System.out.print(value + " ");
+                }
+                System.out.println();
+            }
+        }
         return "index";
     }
 
@@ -35,18 +69,13 @@ public class SteampoweredController {
 	@RequestMapping(value="/redirect", method = RequestMethod.GET)
     public ModelAndView runManager(){
         OpenIdManager manager = new OpenIdManager();
+        // TODO  change these from localhost after uploading the project to a hosting site
         manager.setReturnTo("http://localhost:8080");
         manager.setRealm("http://localhost:8080");
 
-        Endpoint endpoint = manager.lookupEndpoint("https://steamcommunity.com/openid/");
-        System.out.println(endpoint);
-
-        Association association = manager.lookupAssociation(endpoint); 
-        System.out.println(association);
-
-        String url = manager.getAuthenticationUrl(endpoint, association);
-        System.out.println("Copy the authentication URL in browser:\n" + url);
-
+        Endpoint endpoint = manager.lookupEndpoint("https://steamcommunity.com/openid/");      
+        Association association = manager.lookupAssociation(endpoint);      
+        String url = manager.getAuthenticationUrl(endpoint, association);        
         return new ModelAndView("redirect:" + url);
     }
 
