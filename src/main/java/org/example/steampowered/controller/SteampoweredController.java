d45af118eca5029a75b446d2e4fee433cdf036ee
Paul Berger <paulberger56@gmail.com>
2024-03-23 14:13:58 -0400

    Add games to the model to be used for updating the wheel

--- src/main/java/org/example/steampowered/controller/SteampoweredController.java
+++ src/main/java/org/example/steampowered/controller/SteampoweredController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,20 +21,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class SteampoweredController {
 
-    List<Game> games = new ArrayList<>();
+    List<Game> games = Arrays.asList(
+        new Game("400", "Portal", "https://cdn.akamai.steamstatic.com/steam/apps/400/header.jpg?t=1699003695", false),
+        new Game("20900", "The Witcher", "https://cdn.akamai.steamstatic.com/steam/apps/20900/header.jpg?t=1700481839", false),
+        new Game("23310", "The Last Remnant", "https://cdn.akamai.steamstatic.com/steam/apps/23310/header.jpg?t=1536078342", false),
+        new Game("8930", "Sid Meyer's Civilization V", "https://cdn.akamai.steamstatic.com/steam/apps/8930/header.jpg?t=1579731804", true)
+    );
 
     @Autowired
     OpenIdService openIdService;
 
     @GetMapping("/")
     public String getIndexPage(HttpServletRequest request, Model model){
-        openIdService.filterOpenIdResults(request);
-        games.add(new Game("400", "Portal", "https://cdn.akamai.steamstatic.com/steam/apps/400/header.jpg?t=1699003695", false));
-        games.add(new Game("20900", "The Witcher", "https://cdn.akamai.steamstatic.com/steam/apps/20900/header.jpg?t=1700481839", false));
-        games.add(new Game("23310", "The Last Remnant", "https://cdn.akamai.steamstatic.com/steam/apps/23310/header.jpg?t=1536078342", false));
-        games.add(new Game("8930", "Sid Meyer's Civilization V", "https://cdn.akamai.steamstatic.com/steam/apps/8930/header.jpg?t=1579731804", true));
+        openIdService.filterOpenIdResults(request);        
         model.addAttribute("games", games);
-
         return "index";
     }
 
