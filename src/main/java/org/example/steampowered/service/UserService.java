5cfd158fba1ae084dac699c6ee9f868b486f798c
Albert <albert.austiniv@gmail.com>
2024-03-27 01:01:19 -0400

    Added to UserRepository and UserService. Added placeholder text to profile.html.

--- src/main/java/org/example/steampowered/service/UserService.java
+++ src/main/java/org/example/steampowered/service/UserService.java
@@ -1,8 +1,28 @@
 package org.example.steampowered.service;
 
+import java.util.List;
+
+import org.example.steampowered.pojo.User;
+import org.example.steampowered.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository = new UserRepository();
+
+    public List<User> getUsers() {
+        return userRepository.users();
+    }
+
+    public void addUser(User user) {
+        userRepository.addUser(user);
+    }
+
+    public User getUser(int index) {
+        return userRepository.getUser(index);
+    }
     
 }
